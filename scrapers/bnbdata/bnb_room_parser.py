84075ade3b0ada766fe8d4bf6b4ebae127ba2674
Mike Preshman <mpresh@gmail.com>
2016-11-12 18:36:28 -0500

    some updates

--- scrapers/bnbdata/bnb_room_parser.py
+++ scrapers/bnbdata/bnb_room_parser.py
@@ -13,22 +13,24 @@ from retrying import retry
 from datetime import datetime, timedelta
 import traceback
 import dryscrape
+import json
+
 
 AIRBNB_LISTING_URL = "https://www.airbnb.com/rooms/{}"
 
 
-def build_pricing_url(property_id, start, end, guests):
+def build_calendar_url(property_id, start, end, guests, key="d306zoyjsyarp7ifhu67rjxn52tv0t20"):
     start = from_datetime_torequest_date_format(start)
     end = from_datetime_torequest_date_format(end)
     
     params = {}
     params["locale"] = "en"
     params["currency"] = "USD"
-    params["key"] = "d306zoyjsyarp7ifhu67rjxn52tv0t20"
-    params["_p3_impression_id"] = "p3_1478914996_rpkXwODnQeHvhYUk"
+    params["key"] = key
+    #params["_p3_impression_id"] = "p3_1478914996_rpkXwODnQeHvhYUk"
     params["check_in"] = start
     params["check_out"] = end
-    params["_parent_request_uuid"] = "795e2598-7da1-41d7-b5ff-dd00b1992ae9"
+    #params["_parent_request_uuid"] = "795e2598-7da1-41d7-b5ff-dd00b1992ae9"
     params["_intents"] = "p3_book_it"
     params["_interaction_type"] = "pageload"
     params["guests"] = guests
@@ -44,19 +46,32 @@ def from_datetime_torequest_date_format(dt):
     return "{}-{}-{}".format(dt.year, dt.month, dt.day)
 
 
-def build_calendar_url(property_id, start, end, guests):
-    start = from_datetime_torequest_date_format(start)
-    end = from_datetime_torequest_date_format(end)
-    url = "https://www.airbnb.com/rooms/{}?check_in={}&guests={}&adults={}&check_out={}".format(property_id,
-                                                                                                start,
-                                                                                                guests,
-                                                                                                guests,
-                                                                                                end)
-    return url
+def get_json_from_script(soup, attrs):
+    attrs["type"] = "application/json"
+    json_data = soup.find("script", attrs=attrs)
+    
+    json_data = json_data.text
+
+    if json_data.startswith("<!--"):
+        json_data = json_data[len("<!--"):]
+
+    if json_data.endswith("-->"):
+        json_data = json_data[:(len("-->") * -1)]
+        
+    json_data = json.loads(json_data)
+    return json_data
 
 
 def get_calendar_info(url):
+    text = requests.get(url).text
+    data = json.loads(text)
+    data = data["pricing_quotes"][0]
+    return data
+
+    
+def get_calendar_info_old(url):
     print("URL", url)
+    data = {}
     session = dryscrape.Session()
     session.visit(url)
     text = session.body()
@@ -65,16 +80,23 @@ def get_calendar_info(url):
         return False
     
     soup = BeautifulSoup(text, 'html.parser')
-    price = soup.find(class_="book-it__price")
-    subtotal_trs = soup.find_all(class_="book-it-panel")
-    #.find("table").find_all("tr")
-    print("FORM", len(subtotal_trs), text.find("Accommodation"))
-    with open("/tmp/accom.txt", "w") as f:
-        f.write(text.encode('utf-8'))
+    with open("/tmp/pricing_page.txt", "w+") as f:
+        print(text)
+        f.write(text.encode("utf-8"))
         
+    price = soup.find(class_="book-it__price")
+    data["price"] = pull_out_first_integer(price.text)    
+
+
+    json_data = get_json_from_script(soup,
+                                     attrs={"data-hypernova-key":
+                                            "p3hero_and_slideshowbundlejs"})
+    pprint(json_data)
+    with open("/tmp/pricing_data.json", "w+") as logfile:
+        pprint(json_data, logfile)
     sys.exit()
-    data = {}
-    data["price"] = pull_out_first_integer(price.text)
+    
+
     return data
     
 
@@ -87,11 +109,11 @@ def split_calendar_dates(start, end):
     return (first_dates, second_dates)
 
 
-def get_calendar_dates(property_id, start, end, guests, minimum):
-    url = build_calendar_url(property_id, start, end, guests)
+def get_calendar_dates(property_id, start, end, guests, minimum, key):
+    url = build_calendar_url(property_id, start, end, guests, key=key)
     result = get_calendar_info(url)
     calendar = {}
-    if result is False:
+    if result["available"] is False:
         first_half, second_half = split_calendar_dates(start, end)
         result_first = get_calendar_dates(property_id, first_half[0], first_half[1], guests, minimum)
         result_second = get_calendar_dates(property_id, second_half[0], second_half[1], guests, minimum)
@@ -103,21 +125,26 @@ def get_calendar_dates(property_id, start, end, guests, minimum):
     return calendar
 
 
-def get_calendar(property_id="4914702", minimum=2):
+def get_calendar(property_id="4914702", minimum=2, key="d306zoyjsyarp7ifhu67rjxn52tv0t20"):
     now = datetime.now()
     start = datetime(year=now.year, month=now.month, day=now.day)
     end = datetime(year=int(start.year) + 1, month=start.month, day=start.day)
     guests = "4"
-    return get_calendar_dates(property_id, start, end, guests, minimum)
+    return get_calendar_dates(property_id, start, end, guests, minimum, key)
 
 
 @retry(wait_fixed=1000)
 def get_listing_info(listing_number):
     listing_url = AIRBNB_LISTING_URL.format(listing_number)
     text = requests.get(listing_url).text
-    data = parse_room_page(text)
+    try:
+        data = parse_room_page(text)
+    except Exception as e:
+        print(e)
     data["listing_number"] = listing_number
-    calendar = get_calendar(property_id=listing_number, minimum=data["availability"])
+    calendar = get_calendar(property_id=listing_number,
+                            minimum=data["availability"],
+                            key=data["key"])
     data["calendar"] = calendar
     return data
 
@@ -130,10 +157,21 @@ def pull_out_first_integer(text):
     return text
 
 
+def get_key_information(soup):
+    attrs = {"id": "_bootstrap-layout-init"}
+    result = soup.find("meta", attrs=attrs).get("content")
+    result = json.loads(result)
+    return result["api_config"]["key"]
+
+
 def parse_room_page(text):
     data = {}
     soup = BeautifulSoup(text, 'html.parser')
-    
+    with open("/tmp/original.txt", "w+") as f:
+        f.write(text.encode("utf-8"))
+
+    data["key"] = get_key_information(soup)
+        
     try:
         
         meta_list =  list(soup.find_all("meta"))
@@ -230,8 +268,7 @@ def parse_room_page(text):
                 
         #data["details"] = details_children
     except Exception as e:
-        print repr(traceback.format_exception(exc_type, exc_value,
-                                              exc_traceback))
+        print(e)
         raise e
 
     return data
@@ -240,14 +277,15 @@ def parse_room_page(text):
 def main():
     property_id = "4914702"
     guests = 4
-    #data = get_listing_info("4914702")
+    data = get_listing_info("4914702")
     #pprint(data)
-    print(get_calendar())
-    #now = datetime.now()
-    #start = datetime(year=now.year, month=now.month, day=now.day)
-    #end = datetime(year=int(start.year) + 1, month=start.month, day=start.day)
-    #url = build_pricing_url(property_id, start, end, guests)
-    #print(url)
+    #print(get_calendar())
+    now = datetime.now()
+    start = datetime(year=now.year, month=now.month, day=now.day)
+    end = datetime(year=int(start.year) + 1, month=start.month, day=start.day)
+    url = build_calendar_url(property_id, start, end, guests)
+    #data = get_calendar_info(url)
+    #pprint(data)
     
 if __name__ == "__main__":
     main()
