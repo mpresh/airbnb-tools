815cf0dca4571d0f54c55e96b3fb01a83dcb32b3
Mike Preshman <mpresh@gmail.com>
2017-07-30 17:49:51 -0400

    Able to filter by zipcode.

--- airbnb/system/listings.py
+++ airbnb/system/listings.py
@@ -2,8 +2,25 @@ from run_scrapy import run_airbnb_spider
 import zipcodes 
 import pprint
 import time
+import geopy
+import parser
+from geopy.geocoders import Nominatim
+import re
+    
+def is_listing_in_zipcode(listing_id, zipcode):
+    latitude, longitude = parser.get_listing_lat_and_long(listing_id)
+    geolocator = Nominatim()
+    location = geolocator.reverse("{}, {}".format(latitude, longitude))
+    mo = re.search("(\d\d\d\d\d), United States of America", location.address)
+    if mo:
+        zipcode = mo.group(1)
+        return zipcode
+    else:
+        print(location.address)
+        return False
 
-def get_listings_by_zipcode(zipcode, state="MA", adults=None, retry=0):
+
+def get_listings_by_zipcode(zipcode, state="MA", adults=None, retry=0, zipcode_filter=True):
     start_url = "https://www.airbnb.com/s/{}--{}--United-States".format(zipcode, state)
     try:
         listings = run_airbnb_spider(start_url=start_url, adults=adults)
@@ -16,6 +33,11 @@ def get_listings_by_zipcode(zipcode, state="MA", adults=None, retry=0):
                                                state=state,
                                                adults=adults,
                                                retry=retry+1)
+
+    print("Listings found in zipcode {}  #={}".format(zipcode, len(listings)))
+    if zipcode_filter:
+        listings = [listing for listing in listings if is_listing_in_zipcode(listing, zipcode)]
+    print("Listings found in zipcode filtered {}  #={}".format(zipcode, len(listings)))
     return listings
 
 
@@ -37,9 +59,9 @@ def get_all_listings(zip_codes_func, adults=None, state="MA"):
 
 
 if __name__ == "__main__":
-    # all_listings = get_all_listings(zipcodes.get_all_cape_cod_zip_codes(), adults=12)
+    all_listings = get_all_listings(zipcodes.get_all_cape_cod_zip_codes, adults=12)
     # all_listings = get_all_listings(zipcodes.get_all_newport_zip_codes(), adults=10, state="RI")
     # all_listings = get_all_listings(zipcodes.get_all_nantucket_zip_codes(), adults=14, state="MA")
-    all_listings = get_all_listings(zipcodes.get_all_marthas_vinyard_zip_codes, adults=16, state="MA")
-    print(len(all_listings))
+    #all_listings = get_all_listings(zipcodes.get_all_marthas_vinyard_zip_codes, adults=16, state="MA")
+    #print(len(all_listings))
     #print(get_listings_by_zipcode("02635", adults=10))
--- airbnb/system/parser.py
+++ airbnb/system/parser.py
@@ -11,7 +11,7 @@ import re
 from retrying import retry
 import traceback
 import json
-
+from lxml import html
 
 AIRBNB_LISTING_URL = "https://www.airbnb.com/rooms/{}"
 
@@ -19,7 +19,7 @@ AIRBNB_LISTING_URL = "https://www.airbnb.com/rooms/{}"
 def get_json_from_script(soup, attrs):
     attrs["type"] = "application/json"
     json_data = soup.find("script", attrs=attrs)
-    
+
     json_data = json_data.text
 
     if json_data.startswith("<!--"):
@@ -31,6 +31,19 @@ def get_json_from_script(soup, attrs):
     json_data = json.loads(json_data)
     return json_data
 
+
+#@retry(wait_fixed=1000)
+def get_listing_lat_and_long(listing_id):
+    listing_url = AIRBNB_LISTING_URL.format(listing_id)
+    text = requests.get(listing_url).text
+    
+    try:
+        data = parse_room_page(text)
+    except Exception as e:
+        print(e)
+        return None
+
+    return (data["lat"], data["lng"])
 
 @retry(wait_fixed=1000)
 def get_listing_info(listing_number):
@@ -75,104 +88,99 @@ def parse_room_page(text):
         data["key"] = get_key_information(soup)
         json_data = get_json_from_script(soup,
                                          attrs={"data-hypernova-key":
-                                                "p3hero_and_slideshowbundlejs"})
-        json_data = json_data["slideshowProps"]["listing"]
-        #pprint(json_data)
-        #print(type(json_data))
-        #print(json_data.keys())
-        data.update(json_data)
-        
-        
-        meta_list =  list(soup.find_all("meta"))
-        for meta in meta_list:
-            try:
-                if "latitude" in meta.get("property"):
-                    data["latitude"] = meta.get("content")
-                if "longitude" in meta.get("property"):
-                    data["longitude"] = meta.get("content")
-                if "airbedandbreakfast:rating" in meta.get("property"):
-                    data["rating"] = meta.get("content")
-            except:
-                pass
-                
-        price = soup.find(class_="book-it").text.lower()
-        start = price.find("per night")
-        price = price[:start].replace("\n", "")
-        mo = re.search("[$](\d*)", price)
-        if mo:
-            price = mo.group(1)
-        data["price"] = price
-        
-        #summary_dls = list(soup.find(class_="summary-component__dls").find("div").children)
-        #location = list(summary_dls[2].children)[0]
-        #reviews = list(summary_dls[2].children)[1]
-        #data["location"] = location.text
-        #reviews = reviews.text
-        #mo = re.search("(\d*) reviews", reviews)
-        #if mo:
-        #    reviews = mo.group(1)
-        #else:
-        #    reviews = None
-        #
-        #data["reviews"] = reviews
+                                                "p3indexbundlejs"})
 
+        json_data = json_data["bootstrapData"]["listing"]
+        keys = json_data.keys()
+        return json_data
         
-        #profile_link = soup.find(class_="host-profile-position-sm").find("img").get("src")
-        #data["profile_img"] = profile_link
-        
-        data["superhost"] = bool(soup.find(class_="superhost"))
-        
-        data["title"] = soup.find(id="listing_name").text
-        
-        
-        summary = soup.find(class_="summary-component")
-        summary_details = summary.find_all(class_="col-sm-3")
-        
-        data["entire_place"] = bool(summary_details[0].find(class_="icon-entire-place"))
-        data["entire_place"] = summary_details[0].text
-        data["number_guests"] = pull_out_first_integer(summary_details[1].text)
-        data["bedrooms"] = pull_out_first_integer(summary_details[2].text)
-        data["beds"] = pull_out_first_integer(summary_details[3].text)
-        
-        
-        
-        details_section = soup.find(class_="details-section")
-        details_div = list(details_section.children)[0]
-        details_div = list(details_div.children)[0]
-        details_div = list(details_div.children)[0]
-        details_div = list(details_div.children)[0]
-        details_children = list(details_div.children)
-        
-        
-        for index, detail in enumerate(details_children):
-            detail_text = detail.text.lower()
-            
-            if "about this listing" in detail_text:
-                data["detail"] = details_children[index + 1]
-        
-            if "business travel" in detail_text:
-                data["business"] = detail_text
-        
-            if "the space" in detail_text:
-                data["space"] = detail_text
-        
-            if "amenities" in detail_text:
-                data["amenities"] = detail_text
-        
-            if "prices" in detail_text:
-                data["prices"] = detail_text
-        
-            if "description" in detail_text:
-                data["description"] = detail_text
-        
-            if "house rules" in detail_text:
-                data["house_rules"] = detail_text
-        
-            if "safety features" in detail_text:
-                data["safety_features"] = detail_text
-        
-            if "availability" in detail_text:
-                data["availability"] = pull_out_first_integer(str(detail_text))
+        #meta_list =  list(soup.find_all("meta"))
+        #for meta in meta_list:
+        #    try:
+        #        if "latitude" in meta.get("property"):
+        #            data["latitude"] = meta.get("content")
+        #        if "longitude" in meta.get("property"):
+        #            data["longitude"] = meta.get("content")
+        #        if "airbedandbreakfast:rating" in meta.get("property"):
+        #            data["rating"] = meta.get("content")
+        #    except:
+        #        pass
+        #        
+        #price = soup.find(class_="book-it").text.lower()
+        #start = price.find("per night")
+        #price = price[:start].replace("\n", "")
+        #mo = re.search("[$](\d*)", price)
+        #if mo:
+        #    price = mo.group(1)
+        #data["price"] = price
+        #
+        ##summary_dls = list(soup.find(class_="summary-component__dls").find("div").children)
+        ##location = list(summary_dls[2].children)[0]
+        ##reviews = list(summary_dls[2].children)[1]
+        ##data["location"] = location.text
+        ##reviews = reviews.text
+        ##mo = re.search("(\d*) reviews", reviews)
+        ##if mo:
+        ##    reviews = mo.group(1)
+        ##else:
+        ##    reviews = None
+        ##
+        ##data["reviews"] = reviews
+        #
+        #
+        ##profile_link = soup.find(class_="host-profile-position-sm").find("img").get("src")
+        ##data["profile_img"] = profile_link
+        #
+        #data["superhost"] = bool(soup.find(class_="superhost"))
+        #
+        #data["title"] = soup.find(id="listing_name").text
+        #
+        #
+        #data["entire_place"] = bool(summary_details[0].find(class_="icon-entire-place"))
+        #data["entire_place"] = summary_details[0].text
+        #data["number_guests"] = pull_out_first_integer(summary_details[1].text)
+        #data["bedrooms"] = pull_out_first_integer(summary_details[2].text)
+        #data["beds"] = pull_out_first_integer(summary_details[3].text)
+        #
+        #
+        #
+        #details_section = soup.find(class_="details-section")
+        #details_div = list(details_section.children)[0]
+        #details_div = list(details_div.children)[0]
+        #details_div = list(details_div.children)[0]
+        #details_div = list(details_div.children)[0]
+        #details_children = list(details_div.children)
+        #
+        #
+        #for index, detail in enumerate(details_children):
+        #    detail_text = detail.text.lower()
+        #    
+        #    if "about this listing" in detail_text:
+        #        data["detail"] = details_children[index + 1]
+        #
+        #    if "business travel" in detail_text:
+        #        data["business"] = detail_text
+        #
+        #    if "the space" in detail_text:
+        #        data["space"] = detail_text
+        #
+        #    if "amenities" in detail_text:
+        #        data["amenities"] = detail_text
+        #
+        #    if "prices" in detail_text:
+        #        data["prices"] = detail_text
+        #
+        #    if "description" in detail_text:
+        #        data["description"] = detail_text
+        #
+        #    if "house rules" in detail_text:
+        #        data["house_rules"] = detail_text
+        #
+        #    if "safety features" in detail_text:
+        #        data["safety_features"] = detail_text
+        #
+        #    if "availability" in detail_text:
+        #        data["availability"] = pull_out_first_integer(str(detail_text))
                 
     except Exception as e:
         print(e)
@@ -183,4 +191,4 @@ def parse_room_page(text):
 
     
 if __name__ == "__main__":
-    main()
+    print(get_listing_lat_and_long("4914702"))
